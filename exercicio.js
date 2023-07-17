f516298816ba35f39824752b17a9b5452c2f1580
Edixon Jose Tovar Rodriguez <erodriguez@Ufinet.com>
2023-07-17 13:52:18 -0300

    Ejercicio 3

---
+++ exercicio.js
@@ -0,0 +1,12 @@
+const dados = {
+    nome : prompt("Digite seu nome:"),
+    sobrenome : prompt("Digite seu sobrenome:"),
+    edade : Number (prompt("Digite sua Edade:")),
+    telefone : Number (prompt("Digite seu telefone:")),
+
+}
+
+console.table(dados);
+
+
+
